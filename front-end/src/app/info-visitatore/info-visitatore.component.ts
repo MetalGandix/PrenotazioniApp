a4f2b1a214d218be82be66e9531a1d434e5e70ef
Maxxolino <54468502+Maxxolino@users.noreply.github.com>
2021-05-14 20:07:22 +0200

    add delete method to visits from user

--- front-end/src/app/info-visitatore/info-visitatore.component.ts
+++ front-end/src/app/info-visitatore/info-visitatore.component.ts
@@ -13,25 +13,40 @@ export class InfoVisitatoreComponent implements OnInit {
 
   visitor: boolean = false
   constructor(private gestioneUtente: GestioneUtenteService, private service: PrenotazioneService) {
-   }
+  }
   utente: User
   visita: Prenotazione[]
   user: User
+  dialog: boolean = false
+  id: number
 
-  ngOnInit(){
-  this.gestioneUtente.findUtenteSingoloLogin(sessionStorage.getItem('username')).subscribe(data => 
-    {
+  ngOnInit() {
+    this.gestioneUtente.findUtenteSingoloLogin(sessionStorage.getItem('username')).subscribe(data => {
       this.utente = data
       console.log(this.utente)
     })
-  console.log("Session storage",sessionStorage.getItem('username'))
-  this.service.prendiVisitaDalVisitatore(sessionStorage.getItem('username')).subscribe(p=>{
-    this.visita = p
-  })
+    console.log("Session storage", sessionStorage.getItem('username'))
+    this.service.prendiVisitaDalVisitatore(sessionStorage.getItem('username')).subscribe(p => {
+      this.visita = p
+    })
   }
 
 
-  setChangeUserDetail(){
+  setChangeUserDetail() {
     this.gestioneUtente.changeUserDetail(this.utente).subscribe()
   }
+
+  takeId(id: number) {
+    console.log(id)
+    this.id = id
+  }
+
+  cancellaVisita() {
+    this.dialog = true
+
+    this.service.deletePrenotazione(this.id).subscribe()
+    location.reload();
+
+  }
 }
+
--- front-end/src/app/prenotazione-visita/prenotazione-visita.component.ts
+++ front-end/src/app/prenotazione-visita/prenotazione-visita.component.ts
@@ -25,7 +25,7 @@ export class PrenotazioneVisitaComponent implements OnInit {
   input: boolean = false
 
   ngOnInit() {
-    
+
     this.visitor = sessionStorage.getItem("Role") === "ROLE_USER"
   }
 
@@ -51,5 +51,4 @@ export class PrenotazioneVisitaComponent implements OnInit {
       this.input = false
     }
   }
-
 }
