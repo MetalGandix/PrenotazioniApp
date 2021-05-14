7c11c18d72cf3c0d662b214f5587e106163b4971
Maxxolino <54468502+Maxxolino@users.noreply.github.com>
2021-05-14 21:36:25 +0200

    Update forgot-password.component.ts

--- front-end/src/app/forgot-password/forgot-password.component.ts
+++ front-end/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GestioneUtenteService } from '../service/gestione-utente.service';
 
 @Component({
@@ -8,12 +9,14 @@ import { GestioneUtenteService } from '../service/gestione-utente.service';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor(private service: GestioneUtenteService) { }
+  constructor(private service: GestioneUtenteService,private router: ActivatedRoute) { }
 
   username: string
-
+  
   ngOnInit(): void {
-
+  this.router.params.subscribe(r=> {
+    let token = r["token"]
+  })
   }
  resetPassword(){
    console.log(this.username)
