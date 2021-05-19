b237e9305deb3eb6a2e0c6a44afaeec0bddc8745
Maxxolino <54468502+Maxxolino@users.noreply.github.com>
2021-05-19 19:10:40 +0200

    update

--- front-end/src/app/class/prenotazione-campo-effettuata.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PrenotazioneCampoEffettuata } from './prenotazione-campo-effettuata';
-
-describe('PrenotazioneCampoEffettuata', () => {
-  it('should create an instance', () => {
-    expect(new PrenotazioneCampoEffettuata()).toBeTruthy();
-  });
-});
